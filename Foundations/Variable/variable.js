ad1a533b89f47f2300a4891984d9f6aca5aff787
Miliotte <ricardomiliotte@gmail.com>
2020-08-12 09:36:50 -0300

    Variable

---
+++ Foundations/Variable/variable.js
@@ -0,0 +1,21 @@
+//nome da variavel é algo muito importante ele será o identificador que poderar ser usado mais tarde.
+// variable name is something very important it will be the identifier that can be used later.
+var idade  = 18
+
+let age = 20
+//para boas praticas o recomedado é sempre o let
+// for best practices the recommended is always let
+
+
+const c = 5
+console.log(idade, age ,c);
+//Para executar o codigo podem esta utilizam o (CodeRum ou Node)
+
+//no console.log irá exibir o a variavel que foi declarada logo no inicio
+// no console.log will display a variable that was declared early on
+
+
+//Esse são os tipos de variaveís que temos no JavaScript 
+//var é seu contexto atual em execução, o qual é a função a qual pertence ou, para variáveis declaradas fora de qualquer função.
+//let  permite que você declare variáveis limitando seu escopo no bloco, instrução, ou em uma expressão na qual ela é usada.
+//const cria uma variável cujo o valor é fixo, ou seja, uma constante somente leitura.
