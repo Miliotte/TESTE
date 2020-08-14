6d3c0495963e64e66ef917241a1efbb367159fd3
Ricardo Miliotte <62629414+Miliotte@users.noreply.github.com>
2020-08-14 12:09:11 -0300

    String

--- Foundations/Strings/string.js
+++ Foundations/Strings/string.js
@@ -2,3 +2,14 @@ const name = 'Miliotte'
 
 console.log(name.charAt(4))
 //Irá mostra o quarto caractere da cont que (o)
+console.log(name.charCodeAt(4))
+//Usado para identificar o codigo do caractere
+
+console.log('Ricardo '.concat(name).concat("!"))
+//o concat é usado para concatenar ou seja juntar 
+
+console.log(name.replace('e', '3'))
+//o replace é usado para subistuir, exemplo: Miliotte passará a ser: Miliotte3
+
+console.log('Ricardo,Miliotte,Cruz'.split(','))
+//o split é usado para gerar um array.Ele irá retonar ['Ricardo','Miliotte','Cruz']
