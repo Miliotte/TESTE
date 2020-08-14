1ec004b91d9d2ea5b57aea48d8d5da3cdfa17342
Ricardo Miliotte <62629414+Miliotte@users.noreply.github.com>
2020-08-14 17:24:34 -0300

    boolean

---
+++ Foundations/Booleanos/booleanos.js
@@ -0,0 +1,34 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1 
+console.log(!!isAtivo)
+
+//Booleanos verdadeiros
+console.log('os verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!'')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+
+//Booleanos verdadeiros
+console.log('os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+console.log('pra finalizar...')
+console.log(!!(''|| null || 0 || ' '))
+
+let name = 'Miliotte'
+console.log(name || 'Desconhecido')
