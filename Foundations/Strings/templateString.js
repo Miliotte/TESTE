276f53a4ca43c8d7b1a0087bcb0563517a015173
Ricardo Miliotte <62629414+Miliotte@users.noreply.github.com>
2020-08-14 16:00:02 -0300

    templateString

---
+++ Foundations/Strings/templateString.js
@@ -0,0 +1,16 @@
+const name = 'Ricardo'
+const concatenacao = 'Olá' + name + '!'
+const template = `
+    Olá
+    ${name}!`
+
+console.log(concatenacao, template)
+
+// expressoes...
+
+console.log(`1 + 1 = ${1 +1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Eii...${up('cuidado')}`)
+
+//toUpperCase = usado para deixar em CapsLock 
