3454b4f38760e2417a444e76ba5ca7a6db203027
Ricardo Miliotte <62629414+Miliotte@users.noreply.github.com>
2020-08-19 09:58:32 -0300

    function2

---
+++ Foundations/function/function2.js
@@ -0,0 +1,19 @@
+const imprimirSoma = function (a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+//arrow function
+const soma = (a,b) => {
+    return a +b
+}
+console.log (soma(2,3))
+
+
+//retorno implícito
+const subtracao = (a,b) => a - b
+console.log(subtracao(2, 3)) 
+
+const imprimir2 = a => console.log(a)
+imprimir2('legal!!!')
